0e7f5defdc56aba6fa22b4698f425734a6608cf6
Toto Lin <toto@linux.com>
2023-09-06 18:23:53 +0800

    Workaround for capstone SSE/AVX madness
    https://github.com/capstone-engine/capstone/issues/1562

--- NaHCO3/passes/transient_insert_memory_logs_pass.py
+++ NaHCO3/passes/transient_insert_memory_logs_pass.py
@@ -8,8 +8,10 @@ from gtirb_capstone.instructions import GtirbInstructionDecoder
 from gtirb_live_register_analysis import LiveRegisterManager
 from capstone_gt import CsInsn, CS_OP_MEM, CS_AC_WRITE
 from capstone_gt.x86 import X86_AVX_CC_INVALID, X86_SSE_CC_INVALID
-from typing import Iterable
+from typing import Iterable, List
 from uuid import UUID
+import copy
+import math
 import functools
 
 
@@ -36,24 +38,30 @@ class TransientInsertMemoryLogsPass(Pass):
                 instructions: Iterable[CsInsn] = self.decoder.get_instructions(block)
                 insertion_offset = 0
                 for idx, instruction in enumerate(instructions):
-                    mem_write_operand = [x for x in instruction.operands if x.type == CS_OP_MEM and x.access & CS_AC_WRITE]
-                    if len(mem_write_operand) != 0:
-                        if instruction.avx_cc == X86_AVX_CC_INVALID and instruction.sse_cc == X86_SSE_CC_INVALID:
-                            # Memory operand is under or equal to 64 bits
+                    if len(instruction.operands) > 0:
+                        if instruction.operands[0].type == CS_OP_MEM and instruction.operands[0].size > 8:
+                            # Workaround for capstone: capstone doesn't correctly identify accesses for
+                            # a lot of SSE/AVX instructions, so we conservatively identify all first
+                            # SSE/AVX memory operands as being written.
+                            mem_write_operand = instruction.operands[0]
+                        else:
+                            mem_write_operand = next(iter(x for x in instruction.operands if
+                                                          x.type == CS_OP_MEM and x.access & CS_AC_WRITE), None)
+
+                        if mem_write_operand is not None:
+                            mem_operand_strs = []
+                            for i in range(math.ceil(mem_write_operand.size / 8)):
+                                op = copy.copy(mem_write_operand.mem)
+                                op.disp += i * 8
+                                mem_operand_strs += [self.__print_mem_operand(instruction, op)]
+                            print(instruction)
                             rewriting_ctx.insert_at(block, insertion_offset, Patch.from_function(
                                 self.reg_manager.allocate_registers(function, block, idx)(
-                                    self.__build_memory_log_patch(
-                                        self.__print_mem_operand(instruction, mem_write_operand[0].mem)
-                                    ))))
-
-                        else:
-                            # AVX/SSE memory operand
-                            # TODO: support AVX instructions
-                            pass
-                    elif instruction.mnemonic == "push" or instruction.mnemonic == "call":
-                        rewriting_ctx.insert_at(block, insertion_offset, Patch.from_function(
-                            self.reg_manager.allocate_registers(function, block, idx)(
-                                self.__build_memory_log_patch("[rsp-8]"))))
+                                    self.__build_memory_log_patch(mem_operand_strs))))
+                        elif instruction.mnemonic == "push" or instruction.mnemonic == "call":
+                            rewriting_ctx.insert_at(block, insertion_offset, Patch.from_function(
+                                self.reg_manager.allocate_registers(function, block, idx)(
+                                    self.__build_memory_log_patch(["[rsp-8]"]))))
 
                     insertion_offset += instruction.size
 
@@ -83,17 +91,25 @@ class TransientInsertMemoryLogsPass(Pass):
         return s
 
     @staticmethod
-    def __build_memory_log_patch(mem_operand: str):
+    def __build_memory_log_patch(mem_operand_strs: List[str]):
         @patch_constraints(x86_syntax=X86Syntax.INTEL, scratch_registers=2)
         def patch(ctx: InsertionContext):
             r1, r2 = ctx.scratch_registers
-            return f"""
-                lea {r1}, {mem_operand}
-                mov {r2}, [memory_history_top]
+
+            store_instructions = ""
+            for mem_operand_str in mem_operand_strs:
+                store_instructions += f"""
+                lea {r1}, {mem_operand_str}
                 mov [{r2}], {r1}
                 mov {r1}, [{r1}]
                 mov [{r2} + 8], {r1}
                 lea {r2}, [{r2} + 16]
+                """
+                pass
+
+            return f"""
+                mov {r2}, [memory_history_top]
+                {store_instructions}
                 mov memory_history_top, {r2}
             """
 
