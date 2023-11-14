7f58976e0bee6aaf9ed3f90d23994f61525a8848
Toto Lin <csl@lunlimited.net>
2023-11-15 01:02:30 +0900

    Replace pushf/popf with lahf/sahf and seto

--- NaHCO3/abi/x86_64.py
+++ NaHCO3/abi/x86_64.py
@@ -68,47 +68,33 @@ class _X86_64_ELF(_X86_64_ELF_BASE):
             register_use: _PatchRegisterAllocation,
             is_leaf_function: bool,
     ) -> Tuple[Iterable[_AsmSnippet], Iterable[_AsmSnippet], Optional[int]]:
-        init_and_switch_to_scratchpad_stack_snippet = _AsmSnippet(f"""
-            mov %rsp, old_rsp
-            lea scratchpad+{SCRATCHPAD_SIZE - 16}, %rsp
-        """)
-        switch_to_scratchpad_stack_snippet = _AsmSnippet(f"""
-            mov %rsp, old_rsp
-            mov scratchpad_rsp, %rsp
-        """)
-        switch_to_original_stack_snippet = _AsmSnippet(f"""
-            mov %rsp, scratchpad_rsp
-            mov old_rsp, %rsp
-        """)
+        prologue: List[_AsmSnippet] = []
+        epilogue: List[_AsmSnippet] = []
+
+        scratchpad_offset = SCRATCHPAD_SIZE // 2
+        for reg in register_use.clobbered_registers:
+            if reg.name == "rflags":
+                continue
+
+            prologue.append(_AsmSnippet(f"mov %{reg}, scratchpad+{scratchpad_offset}"))
+            epilogue.append(_AsmSnippet(f"mov scratchpad+{scratchpad_offset}, %{reg}"))
+            scratchpad_offset += 8
 
         if constraints.clobbers_flags:
-            # Use switch-stack implementation.
-            new_constraints = copy.copy(constraints)
-            new_constraints.align_stack = False
-
-            new_register_use = copy.copy(register_use)
-            new_register_use.clobbered_registers = [x for x in new_register_use.clobbered_registers
-                                                    if x.name != "rflags"]
-
-            prologue, epilogue, _ = super()._create_prologue_and_epilogue(
-                new_constraints, new_register_use, False)
-            epilogue = list(reversed(list(epilogue)))
-
-            prologue.insert(0, init_and_switch_to_scratchpad_stack_snippet)
-            prologue.append(switch_to_original_stack_snippet)
-            epilogue.insert(0, switch_to_original_stack_snippet)
-            epilogue.append(switch_to_scratchpad_stack_snippet)
-        else:
-            prologue: List[_AsmSnippet] = []
-            epilogue: List[_AsmSnippet] = []
-
-            scratchpad_offset = SCRATCHPAD_SIZE // 2
-            for reg in register_use.clobbered_registers:
-                if reg.name == "rflags":
-                    continue
-
-                prologue.append(_AsmSnippet(f"mov %{reg}, scratchpad+{scratchpad_offset}"))
-                epilogue.append(_AsmSnippet(f"mov scratchpad+{scratchpad_offset}, %{reg}"))
-                scratchpad_offset += 8
+            prologue.append(_AsmSnippet(f"""
+                mov %rax, scratchpad+{scratchpad_offset+8}
+                lahf
+                seto %al
+                mov %rax, scratchpad+{scratchpad_offset}
+                mov scratchpad+{scratchpad_offset+8}, %rax
+            """))
+            epilogue.append(_AsmSnippet(f"""
+                mov %rax, scratchpad+{scratchpad_offset+8}
+                mov scratchpad+{scratchpad_offset}, %rax
+                add $0x7f, %al
+                sahf
+                mov scratchpad+{scratchpad_offset+8}, %rax
+            """))
+            scratchpad_offset += 16
 
         return prologue, reversed(epilogue), None
