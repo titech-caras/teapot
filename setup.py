fdd51416029a09ebceced15295f40185a8fbf15c
Fangzheng Lin <toto@linux.com>
2024-05-17 17:01:53 +0900

    Make teapot installable as a package

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 setup(
     name='teapot',
     version='0.0.1',
+    python_requires='>=3.8,<=3.10',
     packages=find_packages(),
     platforms='any',
     install_requires=REQUIREMENTS,
